635381523b06efe36400559d8c2fdab16cb6ff5f
Veli-Matti Valonen <veli-matti.valonen@metatavu.fi>
2021-08-30 10:34:42 +0300

    restyled the drawer

--- src/components/generics/user-info/user-info.tsx
+++ src/components/generics/user-info/user-info.tsx
@@ -3,9 +3,6 @@ import { Avatar, Box, Typography } from "@material-ui/core";
 import useUserInfoStyles from "styles/generics/user-info/user-info";
 import { selectPerson } from "features/person/person-slice";
 import { useAppSelector } from "app/hooks";
-import strings from "localization/strings";
-import theme from "theme/theme";
-import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 /**
  * User info component
@@ -17,36 +14,6 @@ const UserInfo: React.FC = () => {
   if (!person) {
     return null;
   }
-
-  /**
-   * Renders the active/inactive status for user
-   * 
-   * @param status user status
-   * @param color color for the status
-   */
-  const renderUserStatus = (status: string, color: string) => {
-    return (
-      <Box style={{ display: "flex", alignItems: "center" }}>
-        <FiberManualRecordIcon
-          htmlColor={ color }
-          style={{
-            width: 6,
-            height: 6
-          }}
-        />
-        <Typography
-          variant="h6"
-          style={{
-            color: color,
-            marginLeft: 4,
-            fontStyle: "italic"
-          }}
-        >
-          { status }
-        </Typography>
-      </Box>
-    );
-  };
 
   /**
    * Renders the username section 
@@ -61,80 +28,15 @@ const UserInfo: React.FC = () => {
           { person.firstName.charAt(0) }
         </Avatar>
         <Box style={{ paddingLeft: 10 }}>
-          <Typography variant="h2">
+          <Typography variant="h1">
             { `${person.firstName} ${person.lastName}` }
           </Typography>
           <Typography
-            variant="h6"
+            variant="body1"
             style={{ color: "rgba(0,0,0,0.6)" }}
           >
             { person.email }
           </Typography>
-        </Box>
-        <Box className={ classes.status }>
-          <Typography
-            variant="h6"
-            style={{
-              color: "rgba(0, 0, 0, 0.6)",
-              fontStyle: "italic"
-            }}
-          >
-            { `${strings.drawerContent.userInfo.id}: ${person.id}` }
-          </Typography>
-          { person.active ?
-            renderUserStatus(strings.drawerContent.userInfo.active, theme.palette.success.main) :
-            renderUserStatus(strings.drawerContent.userInfo.inactive, theme.palette.error.main)
-          }
-        </Box>
-      </>
-    );
-  };
-
-  /**
-   * Renders the username section 
-   * 
-   * @param name name of the subtitle text
-   * @param value value of the subtitle text
-   * @param variant variant of the subtitle text
-   */
-  const renderUserDetailEntry = (name: string, value: string, variant: "h5" | "h6") => {
-    return (
-      <>
-        <Typography
-          variant={ variant }
-          style={{ fontWeight: 600 }}
-        >
-          { name }
-        </Typography>
-        <Typography
-          variant={ variant }
-          className={ classes.infoValue }
-        >
-          { value }
-        </Typography>
-      </>
-    );
-  };
-
-  /**
-   * Renders the user detail section 
-   */
-  const renderUserDetail = () => {
-    return (
-      <>
-        <Box className={ classes.userDetailEntry }>
-          { renderUserDetailEntry(strings.drawerContent.userInfo.userType, person.userType, "h5") }
-        </Box>
-        <Box className={ classes.userDetailEntry }>
-          { renderUserDetailEntry(strings.drawerContent.userInfo.language, person.language, "h5") }
-        </Box>
-        <Box className={ classes.date }>
-          <Box className={ classes.userDetailDateEntry }>
-            { renderUserDetailEntry(strings.drawerContent.userInfo.createdAt, person.createdAt.toLocaleString(), "h6") }
-          </Box>
-          <Box className={ classes.userDetailDateEntry }>
-            { renderUserDetailEntry(strings.drawerContent.userInfo.updatedAt, person.updatedAt.toLocaleString(), "h6") }
-          </Box>
         </Box>
       </>
     );
@@ -147,9 +49,6 @@ const UserInfo: React.FC = () => {
     <Box className={ classes.root }>
       <Box className={ classes.userNameContainer }>
         { renderUsername() }
-      </Box>
-      <Box className={ classes.userDetail }>
-        { renderUserDetail() }
       </Box>
     </Box>
   );
