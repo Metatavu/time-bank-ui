00eaf10608e35dce7c249b41b7f933111f8dc948
villejuutila <cviju002@edu.xamk.fi>
2022-07-28 11:41:11 +0300

    Generated files removed

--- src/generated/client/apis/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './DailyEntriesApi';
-export * from './PersonsApi';
-export * from './SynchronizeApi';
-export * from './SystemApi';
-export * from './TimeEntriesApi';
--- src/generated/client/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './DailyEntry';
-export * from './ModelError';
-export * from './Person';
-export * from './PersonTotalTime';
-export * from './TimeEntry';
-export * from './Timespan';
